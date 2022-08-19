8b67a455a8ee5d6f959c9096efb30e86ca7116db
sudo-shashank <shashank@chainsafe.io>
2022-08-19 14:41:56 +0530

    Added key read

--- rusty-editor/src/main.rs
+++ rusty-editor/src/main.rs
@@ -1,7 +1,50 @@
 mod clear;
 use clear::*;
+mod read;
+use crossterm::event::{
+    KeyboardEnhancementFlags, PopKeyboardEnhancementFlags, PushKeyboardEnhancementFlags,
+};
+use crossterm::{
+    event::{
+        DisableBracketedPaste, DisableFocusChange, DisableMouseCapture, EnableBracketedPaste,
+        EnableFocusChange, EnableMouseCapture,
+    },
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode},
+    Result,
+};
+use read::*;
+use std::io::stdout;
 
-fn main() {
+fn main() -> Result<()> {
     let _ = clear_screen();
-    println!("Hello, Terminal!");
+    println!("Welcome to, Rusty Editor!\n");
+    enable_raw_mode()?;
+
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        EnableBracketedPaste,
+        EnableFocusChange,
+        EnableMouseCapture,
+        PushKeyboardEnhancementFlags(
+            KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES
+                | KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES
+                | KeyboardEnhancementFlags::REPORT_EVENT_TYPES
+        )
+    )?;
+
+    if let Err(e) = read_event() {
+        println!("Error: {:?}\r", e);
+    }
+
+    execute!(
+        stdout,
+        DisableBracketedPaste,
+        PopKeyboardEnhancementFlags,
+        DisableFocusChange,
+        DisableMouseCapture
+    )?;
+
+    disable_raw_mode()
 }
