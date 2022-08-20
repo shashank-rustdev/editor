3d4d4c43a8b4b0599ea9c68be86c7908634b05b3
sudo-shashank <shashank@chainsafe.io>
2022-08-20 08:07:15 +0530

    Added config values

--- rusty-editor/src/clear.rs
+++
@@ -1,9 +0,0 @@
-use crossterm::cursor;
-use crossterm::{execute, terminal, terminal::ClearType};
-use std::io::stdout;
-
-// Clear Screen and reset the cursor position
-pub fn clear_screen() -> crossterm::Result<()> {
-    execute!(stdout(), terminal::Clear(ClearType::All))?;
-    execute!(stdout(), cursor::MoveTo(0, 0))
-}
---
+++ rusty-editor/src/config.rs
@@ -0,0 +1,3 @@
+pub const VERSION: &str = "0.0.1";
+pub const TAB_STOP: usize = 8;
+pub const QUIT_TIMES: u8 = 3;
